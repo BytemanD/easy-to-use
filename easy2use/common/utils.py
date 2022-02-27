560ce4b51aec5a98666f95e339e76221217a9d94
zbw <2535463841@qq.com>
2022-02-27 14:44:14 +0800

    Update

--- easy2use/common/utils.py
+++ easy2use/common/utils.py
@@ -1,6 +1,16 @@
 import importlib
+import pkg_resources
 
 
 def import_class(class_path):
     mod, _, klass = class_path.rpartition('.')
     return getattr(importlib.import_module(mod), klass)
+
+
+def package_version(name):
+    try:
+        version = pkg_resources.get_distribution(name).version
+    except pkg_resources.DistributionNotFound:
+        version = None
+
+    return version
